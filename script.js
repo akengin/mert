4ab19340beba25b4eb1adb83d5899166a114e0d9
Mert Akengin <5755154+spacelatte@users.noreply.github.com>
2023-11-23 12:55:04 +0100

    worker+scripts: refactor file extension discovery mechanism

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 "use strict";
 
 let _scroll = {
-	load: function(storage) {
+
+	load: function (storage) {
 		let data = JSON.parse(storage.getItem("scroll") || "[0,0]")
 		return window.scrollTo(data[0], data[1])
 	},
-	save: function(storage) {
+
+	save: function (storage) {
 		let data = JSON.stringify([ window.scrollX, window.scrollY, ])
 		return storage.setItem("scroll", data)
 	},
+
 }
 
 
@@ -18,62 +21,79 @@ function withSelf(self, callback) {
 }
 
 function makeWorker(script, callback) {
-	if("Worker" in window && window.Worker) {
+
+	if ("Worker" in window && window.Worker) {
 		let worker = new Worker(script)
+		if(callback) callback.selfWorker = worker
 		worker.onmessage = callback
 		return worker
 	}
+
 	return null
 }
 function developerMode(hostname) {
+
 	let storage = window.sessionStorage;
-	if(window.location.hostname === hostname) {
+
+	if (window.location.hostname === hostname) {
 		// window.location.href.replace(/^http/, "ws")
 		new WebSocket(`ws://${window.location.host}/`).onmessage = function() {
-			console.debug(arguments, _scroll.save(sessionStorage))
+			console.debug("developerMode/hostname/WebSocket:", arguments, _scroll.save(sessionStorage))
 			return window.location.reload(true)
 		}
 	}
 	return
 }
-function loadDocument(worker) {
-	let directory = "/pages/"
+function loadDocument(worker, extension = null) {
+
+	const directory = "/pages/"
+
 	let target = window.location.search.substring(1)
-	if(!target) {
+
+	if (!target) {
 		target = directory
 	}
-	if(!target.startsWith(directory)) {
+
+	if (!target.startsWith(directory)) {
 		target = `${directory}${target}`
 	}
-	if(target.endsWith("/")) {
-		target += "home.md"
+
+	if (target.endsWith("/")) {
+		target += "home"
 	}
-	if(!target.endsWith(".md")) {
-		target += ".md"
-	}
+
 	return worker.loader.postMessage({
+		extension: extension,
 		element: "main#content",
 		file: target,
 	})
 }
 
 function onHashChange(event) {
+
 	if(event) event.preventDefault()
+
 	let absolute = true
 	let offsetMultiplier = 1.1
-	if(window.location.hash.substring(1)) {
+
+	if (window.location.hash.substring(1)) {
+
 		let docRect = document.body.getBoundingClientRect()
 		let section = document.querySelector(window.location.hash.replace("~", ""))
 		let titleRect = document.querySelector("#title").getBoundingClientRect()
-		if(section && section instanceof HTMLHeadingElement) {
+
+		if (section && section instanceof HTMLHeadingElement) {
+
 			let secRect = section.getBoundingClientRect()
+
 			console.log(
 				"scroll:", window.scrollY,
 				"section", secRect.top,
 				"doc:", docRect.top,
 				"event:", event,
 			)
-			if(absolute) {
+
+			if (absolute) {
 				window.scrollTo({
 					top: secRect.top - titleRect.height * offsetMultiplier - docRect.top,
 					left: 0,
@@ -93,23 +113,28 @@ function onHashChange(event) {
 		// 	block: "start",
 		// })
 	} else _scroll.load(sessionStorage);
+
 	return false
 }
 
 function onLoad(event) {
-	console.debug(arguments)
+
+	console.debug("onLoad:", arguments)
+
 	try {
 		developerMode("localhost");
 	} catch(e) {
 		console.log("DevModeError", e)
 	}
-	if("serviceWorker" in navigator) {
+
+	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker.register("./workers/service.js")
-			.then(result => console.debug(result))
-			.catch(error => console.error(error))
+			.then(result => console.debug("onLoad/serviceWorker/result:", result))
+			.catch(error => console.error("onLoad/serviceWorker/error:",  error))
 	}
+
 	let parser = makeWorker("./workers/markdown.js", function(e) {
-		console.debug(arguments)
+		console.debug("parser/makeWorker/markdown.js:", arguments)
 		let element = e.data.target
 		if(typeof(element) === "string")
 			element = document.querySelector(element)
@@ -138,34 +163,67 @@ function onLoad(event) {
 		onHashChange(null)
 		return
 	})
-	let loader = makeWorker("./workers/fetch.js", function(e) {
-		console.debug(arguments)
-		return parser.postMessage({
-			target: e.data.element,
-			text: e.data.content,
+
+	let loader = makeWorker("./workers/fetch.js", function (event) {
+
+		try {
+			console.debug("loader/makeWorker/fetch.js:", arguments)
+		} catch (error) {
+			console.warn("loader/makeWorker/fetch.js:", error)
+		}
+		const data = event.data
+		const extensions = [".md", ".htm", ".html", ".txt", ".json", ".js",]
+		const extension = (event.data?.extension || extensions[0])
+		const nextExtensionIndex = extensions.indexOf(extension) + 1
+		const nextExtension = extensions[nextExtensionIndex]
+
+		if (true
+			&& data.http
+			&& !data.http.ok
+			&& !data.file.endsWith(extension)
+			&& !data.file.endsWith(nextExtension)
+			&& nextExtensionIndex < extensions.length
+		) {
+			console.debug("loader/makeWorker/fetch.js/callback:", "Trying next extension", data, nextExtension)
+			return this.postMessage({
+				extension: nextExtension,
+				element: data.element,
+				file: data.file,
+			})
+		} else return parser.postMessage({
+			target: data.element,
+			text: data.content,
 		})
 	})
+
 	document.querySelectorAll("[data-load]").forEach(function(e, i) {
+
 		console.log(e, i, arguments)
+
 		e.dataset.identity = Array(32)
 			.fill(0)
 			.map(x => Math.random().toString(36).charAt(2))
 			.join("")
+
 		e.classList.add("loading")
-		return loader.postMessage({
+
+		if (loader) return loader.postMessage({
+			extension: null,
 			element: `[data-identity="${e.dataset.identity}"]`,
 			file: e.dataset.load,
 		})
+
 		return fetch(e.dataset.load)
 			.then(resp => resp.text())
 			.then(html => {
 				e.innerHTML = html;
 			})
 	})
+
 	return loadDocument({
 		parser,
 		loader,
-	})
+	}, ".md")
 }
 
 window.addEventListener("load", onLoad, true)
--- workers/fetch.js
+++ workers/fetch.js
@@ -1,16 +1,68 @@
 
 "use strict";
 
-onmessage = function(e) {
-	console.debug(arguments)
-	fetch(e.data.file)
-		.then(resp => resp.text())
-		.then(text => postMessage({
-			element: e.data.element,
-			content: text,
-		}))
-		.catch(err => postMessage({
-			element: e.data.element,
-			content: err,
-		}))
+const simpleResponse = false
+
+// reply: proxy function for postMessage, returns original argument back
+async function reply(response) {
+	let result = postMessage(response)
+	if (result) return result
+	return response
+}
+
+onmessage = async function (event) {
+
+	try {
+		console.debug("fetch.js/onmessage:", arguments)
+	} catch (error) {
+		console.warn("fetch.js/onmessage:", error)
+	}
+
+	const extension = event.data?.extension
+	const element = event.data.element
+
+	let file = event.data.file
+
+	if (file.endsWith("/")) {
+		file = `${event.data.file}/index`
+	}
+
+	if (extension && !file.endsWith(extension)) {
+		file += extension
+	}
+
+	if (simpleResponse) return await reply({
+		extension: extension,
+		element: element,
+		content: (await (await fetch(file)).text()),
+		file: event.data.file, // pass original
+	})
+
+	return console.log("bok:",
+			await fetch(file)
+				.then(async response => ({
+					resp: response,
+					text: await response.text(),
+				}))
+				.then(async obj => await reply({
+					extension: extension,
+					element: element,
+					content: obj.text,
+					file: event.data.file, // pass original
+					http: {
+						//headers: obj.resp.headers,
+						bodyUsed: obj.resp.bodyUsed,
+						ok: obj.resp.ok,
+						redirected: obj.resp.redirected,
+						status: obj.resp.status,
+						statusText: obj.resp.statusText,
+						type: obj.resp.type,
+						url: obj.resp.url,
+					},
+				}))
+				.catch(async err => await reply({
+					element: event.data.element,
+					content: err,
+				}))
+		)
 }
